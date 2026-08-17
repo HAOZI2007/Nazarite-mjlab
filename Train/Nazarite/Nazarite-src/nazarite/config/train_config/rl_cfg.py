ff5bcaff7fb052e44cbdadd16d7cbc755e7b498d
HAOZI2007 <1459772301@qq.com>
2026-08-17 17:14:44 +0800

    更新训练配置文件

---
+++ Train/Nazarite/Nazarite-src/nazarite/config/train_config/rl_cfg.py
@@ -0,0 +1,49 @@
+from mjlab.rl import (
+  RslRlModelCfg,
+  RslRlOnPolicyRunnerCfg,
+  RslRlPpoAlgorithmCfg,
+)
+
+
+
+def unitree_go2_normal_ppo_runner_cfg(
+  experiment_name: str = "go2_velocity",
+) -> RslRlOnPolicyRunnerCfg:
+  """Create RL runner configuration for Unitree Go2 velocity task."""
+  return RslRlOnPolicyRunnerCfg(
+    actor=RslRlModelCfg(
+      class_name="MLPModel",
+      hidden_dims=(512, 256, 128),
+      activation="elu",
+      obs_normalization=False,
+      distribution_cfg={
+        "class_name": "GaussianDistribution",
+        "init_std": 1.0,
+        "std_type": "log",
+      },
+    ),
+    critic=RslRlModelCfg(
+      class_name="MLPModel",
+      hidden_dims=(512, 256, 128),
+      activation="elu",
+      obs_normalization=False,
+    ),
+    algorithm=RslRlPpoAlgorithmCfg(
+      value_loss_coef=1.0,
+      use_clipped_value_loss=True,
+      clip_param=0.2,
+      entropy_coef=0.005,
+      num_learning_epochs=5,
+      num_mini_batches=4,
+      learning_rate=1.0e-5,
+      schedule="adaptive",
+      gamma=0.99,
+      lam=0.95,
+      desired_kl=0.01,
+      max_grad_norm=1.0,
+    ),
+    experiment_name=experiment_name,
+    save_interval=50,
+    num_steps_per_env=24,
+    max_iterations=15_000,
+  )
